f53ad37989b2260db82ebe7bab6c9588b719c4d8
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-29 00:41:25 -0400

    Sebastian B

--- exercise3.py
+++ exercise3.py
@@ -1,49 +1,30 @@
-#GitHub Name: LEMcD
-#Course ID:  COP2002, Section:  0M1
-#Date Created:  September 8, 2024
-#Program Title:  Exercise 2
-#Description:  Using variables, assignment statements, calculations, and different ways of printing them
+## Sebastian Brant
+## F24 COP2002.0M1
+## 9/22/24
 
-#Creat variables for introduction sentences
+def main():
+    #Two lists, for hex codes and manufacturer
+    hex_codes=["00:00:17","00:07:E9","04:27:28","04:26:65","04:33:89","00:00:0C"]
+    manufacturer=["Oracle","Intel Corporation","Microsoft Corporation","Apple, Inc.","Huawei Technologies Co.,Ltd","Cisco Systems, Inc"]
 
-name="Leigh McDavitt"
-major="Information Systems Technology-Security concentration"
-interest="I get to learn Python Langauage"
+    #Input for user, which then runs input through if, elif, else, checking the hex_codes list and printing the corresponding manufacturer
+    print("MAC Manufacturer Program\n------------------------\n")
+    hex_code_input=input("Enter the first 6 values of the MAC address (format as XX:XX:XX): ")
+    if hex_code_input==hex_codes[0]:
+        print(manufacturer[0])
+    elif hex_code_input==hex_codes[1]:
+        print(manufacturer[1])
+    elif hex_code_input==hex_codes[2]:
+        print(manufacturer[2])
+    elif hex_code_input==hex_codes[3]:
+        print(manufacturer[3])
+    elif hex_code_input==hex_codes[4]:
+        print(manufacturer[4])
+    elif hex_code_input==hex_codes[5]:
+        print(manufacturer[5])
+    else:
+        print("Unknown")
 
-
-#Print introduction string sentences
-
-print("My name is "+(name)+"." "  My major is " +(major)+".")
-print("I'm most interested in this class because "+(interest)+"."+"\n")
-
-#Create variables for numbers used in calculations
-
-num1=28
-num2=37
-
-#Create variables for calculations using the number variables already defined & print the results
-
-sum=num1+num2 #variable for addtion 
-answer1=str(num1)+" "+"+"+" "+str(num2)+" "+"="" "+str(sum)+"\n" #variable to print entire addition equation with spaces
-print(answer1)
-
-
-difference=num1-num2 #variable for subraction 
-answer2=str(num1)+" "+"-"+" "+str(num2)+" "+"="" "+str(difference)+"\n" #variable to print entire subraction equation with spaces
-print(answer2)
-      
-product=num1*num2 #variable for multiplication 
-answer3=str(num1)+" "+"*"+" "+str(num2)+" "+"="" "+str(product)+"\n" #variable to print entire multiplication equation with spaces
-print(answer3)
-
-quotient=num1/num2 #variable for division 
-answer4=str(num1)+" "+"/"+" "+str(num2)+" "+"="" "+str(quotient)+"\n" #variable to print entire division equation with spaces
-print(answer4)
-      
-modulus=num1%num2 #variable for remainder
-answer5=str(num1)+" "+"%"+" "+str(num2)+" "+"="" "+str(modulus)+"\n\n" #variable to print entire modulus equation with spaces
-print(answer5)
-
-#Create variable and print end message of program
-ending="This is the end of the program!"
-print(ending)
+#Main function check
+if __name__=="__main__":
+    main()
