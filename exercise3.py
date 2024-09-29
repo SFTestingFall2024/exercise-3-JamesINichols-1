01642d6da25ed2eca2901dc6ad05d1bd20fd1c29
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-28 21:22:03 -0400

    Tyler S

--- exercise3.py
+++ exercise3.py
@@ -1,28 +1,64 @@
+# Tyler Santos (santost12)
+# COP2002-OM1
+# September 18, 2024
+# Exercise 3: If statements
+# Program to determine manufacturer for a NIC card
+
+
+# Takes macInput string and looks up manufacturer.
+def lookupMac(macInput):
+    macInput = macInput.upper() # Convert to uppercase
+
+    if macInput == "00:00:17":
+        print(f"For {macInput} the MAC manufacturer is Oracle.")
+
+    elif macInput == "00:07:E9":
+        print(f"For {macInput} the MAC manufacturer is Intel.")
+
+    elif macInput == "04:27:28":
+        print(f"For {macInput} the MAC manufacturer is Microsoft Corporation.")
+
+    elif macInput == "04:26:65":
+        print(f"For {macInput} the MAC manufacturer is Apple, Inc.")
+
+    elif macInput == "04:33:89":
+        print(f"For {macInput} the MAC manufacturer is Huawei Technologies Co.,Ltd.")
+
+    elif macInput == "00:00:0C":
+        print(f"For {macInput} the MAC manufacturer is Cisco Systems, Inc.")
+
+    else:
+        print(f"For {macInput} the MAC manufacturer is Unknown.")
+
+
+# Takes macInput string and checks if is 8 characters.
+# Returns 0 for valid input.
+def validateInputLen(macInput):
+    if len(macInput) < 8:
+        print("Error: Not enough characters entered.")
+        return -1
+
+    elif len(macInput) > 8:
+        print("Error: Too many characters entered.")
+        return -2 
+
+    else:
+        return 0
+
+
+# Main function, where the user is prompted for input.
 def main():
-    # Dictionary to store MAC address prefixes and their manufacturers
-    mac_manufacturers = {
-        "00:00:17": "Oracle",
-        "00:07:E9": "Intel Corporation",
-        "04:27:28": "Microsoft Corporation",
-        "04:26:65": "Apple, Inc.",
-        "04:33:89": "Huawei Technologies Co.,Ltd",
-        "00:00:0C": "Cisco Systems, Inc"
-    }
-
-    # Print the title
     print("MAC Manufacturer Program")
-    print("------------------------")
-    print()
+    print("------------------------\n")
 
-    # Get user input
-    user_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ").strip()
+    macInput = str(input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): "))
 
-    # Determine the manufacturer or if it's unknown
-    manufacturer = mac_manufacturers.get(user_input, "Unknown")
+    # Require 8 characters
+    while (validateInputLen(macInput) != 0):
+        macInput = str(input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): "))
 
-    # Print the result
-    print(f"For {user_input} the MAC manufacturer is {manufacturer}.")
+    lookupMac(macInput)
 
-# Call the main function
-if __name__ == "__main__":
+
+if (__name__=="__main__"):
     main()
