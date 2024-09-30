e7b67f1bce91f74b497a3f55d0afe0de120756e1
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-30 15:11:51 -0400

    Frank E

--- exercise3.py
+++ exercise3.py
@@ -1,26 +1,34 @@
-# Zane Vance
-# COP2002.0M1: Program Logic
-# Created 9/22/24
-# Exercise 3: If statements
-# Program will take user input to discern which manufacturer made an item from the first 6 hex values of the MAC address
+# dictionary of manufacturers
+manufacturers = {
+    "00:00:17": "Oracle",
+    "00:07:E9": "Intel Corporation",
+    "04:27:28": "Microsoft Corporation",
+    "04:26:65": "Apple, Inc.",
+    "04:33:89": "Huawei Technologies Co.,Ltd",
+    "00:00:0C": "Cisco Systems, Inc"
+}
 
-print("MAC Manufacturer Program")
-print("------------------------\n")
+def get_manufacturer(oui):
+    """Retrieve the manufacturer based on the OUI."""
+    return manufacturers.get(oui.upper(), "Manufacturer not found")
 
-# Prints the top of the program. Number of dashes might be off.
+def main():
+    print("NIC Card Manufacturer Lookup")
+    
+    # Prompt user for input
+    user_input = input("Enter the first 6 hex digits formatted as XX:XX:XX: ")
 
-hex = input("Enter the first 6 hex values  of the MAC address (format as XX:XX:XX): ")
+    # Validate the input format
+    if (len(user_input) == 8 and
+        user_input[2] == ':' and
+        user_input[5] == ':' and
+        all(c in '0123456789ABCDEFabcdef' for c in user_input.replace(':', ''))):
+        
+        manufacturer = get_manufacturer(user_input[:8])  # Extract the OUI
+        print("\nResult:")
+        print(f"Manufacturer: {manufacturer}")
+    else:
+        print("Invalid format. Please enter the OUI in the format XX:XX:XX.")
 
-# Prompts the user for the 6 hex values
-
-if hex== "00:00:17" : manufacturer = "Oracle"
-elif hex== "00:07:E9" : manufacturer = "Intel Corporation"
-elif hex== "04:27:28" : manufacturer = "Microsoft Corporation"
-elif hex== "04:26:65" : manufacturer = "Apple, Inc."
-elif hex== "04:33:89" : manufacturer = "Huawei Technologies Co.,Ltd"
-elif hex=="00:00:0C" : manufacturer = "Cisco Systems, Inc"
-else : manufacturer = "Unknown"
-
-# Compares the user-inputted hex value to the ones tied to given manufacturers
-
-print(f"For " + hex +" the MAC manufacturer is "+ manufacturer+ ".")
+if __name__ == "__main__":
+    main()
