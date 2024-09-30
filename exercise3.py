5aa0f4d50e82fedd822530cf87364370de1fa900
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-30 06:53:07 -0400

    Emma W

--- exercise3.py
+++ exercise3.py
@@ -1,35 +1,36 @@
-# Name: Dalton Lundy
-# Course ID: F24 COP2002.0M1
-# Date Created: 9/22/24
-# Program Title: Exercise 3
-# Program Description: This program determines the manufacturer of a NIC card based on the first 6 hex digits of the MAC address.
+# Emma Whitworth (EmmaBean004)
+# COP2002-0T1
+# September 22,2024
+# MAC Finder
+# Identifies the manufacturer of a given MAC
 
 def main():
-    # List of manufacturers
-    manufacturers = ["Oracle","Intel Corporation","Microsoft Corporation","Apple, Inc.","Huawei Technologies Co.,Ltd","Cisco Systems, Inc"]
-
-    # Prompt the user for input
-    user_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
-
-    # Determine the manufacturer using if statements
-    if user_input == "00:00:17":
-        manufacturer = manufacturers[0]
-    elif user_input == "00:07:E9":
-        manufacturer = manufacturers[1]
-    elif user_input == "04:27:28":
-        manufacturer = manufacturers[2]
-    elif user_input == "04:26:65":
-        manufacturer = manufacturers[3]
-    elif user_input == "04:33:89":
-        manufacturer = manufacturers[4]
-    elif user_input == "00:00:0C":
-        manufacturer = manufacturers[5]
-    else:
-        manufacturer = "Unknown"
-
-    # Print the result
-    print(f"\nFor {user_input} the MAC manufacturer is {manufacturer}.")
-
-
+    # Indent code you write to go in here
+    Hexlist = ["00:00:17",
+               "00:07:E9",
+               "04:27:28",
+               "04:26:65",
+               "04:33:89",
+               "00:00:0C",
+               "<Not valid value or not found>"]
+    Manlist = ["Oracle",
+                    "Intel Corporation",
+                    "Microsoft Corporation",
+                    "Apple,Inc.",
+                    "Huawei Technologies Co.,Ltd",
+                    "Cisco Systems, Inc",
+                    "Unknown"]
+    UserHex = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+    n = 0
+    while (n<6):
+        if(UserHex==Hexlist[n]):
+            print("For "+UserHex+" the MAC manufacturer is "+Manlist[n])
+            break
+        else:
+            n+=1
+    if (n>=6):
+        print("For "+Hexlist[n]+" the MAC manufacturer is "+Manlist[n])
+    
 if(__name__=="__main__"):
+    print("MAC Manufacturer Program \n")
     main()
