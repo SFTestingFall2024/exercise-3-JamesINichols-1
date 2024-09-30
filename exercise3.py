2386054eaac55338b93e2993491d1b414acfdd68
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-30 07:16:03 -0400

    Jason D

--- exercise3.py
+++ exercise3.py
@@ -1,30 +1,30 @@
-#ChristianMathieu
-#COP2002.0M1
-#September 22,2024
-#NIC Manufacturer Identifier
-#Identifies Manacturer using hex codes
-print("Mac Manufacturer Identifier" + " " *2)
+# Jason DeGroff (JasonDeGroff)
+# COP2002-0M1
+# September 22,2024
+# MAC Manufacturer Program
+# This program takes user input in the format of XX:XX:XX hex values on a NIC card and provides the manufacturer of the card.
+
 def main():
-    # List of hex codes and their  manufacturers
-    manufacturers = {
-        "00:00:17": "Oracle",
-        "00:07:E9": "Intel Corporation",
-        "04:27:28": "Microsoft Corporation",
-        "04:26:65": "Apple, Inc.",
-        "04:33:89": "Huawei Technologies Co.,Ltd",
-        "00:00:0C": "Cisco Systems, Inc"
-    }
+    # Set list of MAC address and Manufactures. These are constants as they should not change during this program
+    MAC_ADDRESSES = ["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C",]
+    MANUFACTURERS = ["Oracle", "Intel Corporation", "Microsoft Corporation", "Apple, Inc.", "Hauwei Technologies Co.,Ltd", "Cisco Systems, Inc", "Unknown"]
+    
+    # Begins display of program and ask for user input
+    print("MAC Manufacturer Program")
+    print("------------------------\n")
+    macAddress = input("Enter the first 6 hex vaules of the MAC address (format as XX:XX:XX): ")
+    
+    #here we use a while loop to go through the MAC_ADDRESSES. If the user input matches the address it leaves the loop, otherwise it itterates the index and trys the next index in the MAC_ADDRESSES list
+    i = 0
+    while i < len(MAC_ADDRESSES):
+        if macAddress == MAC_ADDRESSES[i]:
+            break
+        i+=1
+    
+    #This prints the manufacture for the user input MAC ADDRESS
+    print(f"For {macAddress} the MAC manufacturer is {MANUFACTURERS[i]}.")
 
-    # Prompt user for input
-    hex_digits = input("Enter the first 6 hex digits (formatted as XX:XX:XX): ")
 
-    # Get the manufacturer, default to "Unknown" if not found
-    manufacturer = manufacturers.get(hex_digits, "Unknown")
-
-    # Output the result
-    print("Hex Digits\tManufacturer")
-    print(f"{hex_digits}\t{manufacturer}")
-
-#  main function
-if __name__ == "__main__":
+    
+if(__name__=="__main__"):
     main()
