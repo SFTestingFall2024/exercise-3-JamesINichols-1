7b662d3bdd5b7f33e0c0f9485c97e2b7a6287fd6
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-29 23:59:02 -0400

    Juan G

--- exercise3.py
+++ exercise3.py
@@ -1,25 +1,30 @@
-def get_manufacturer(hex_digits):
-    manufacturers = {
-        "00:00:17": "Oracle",
-        "00:07:E9": "Intel Corporation",
-        "04:27:28": "Microsoft Corporation",
-        "04:26:65": "Apple, Inc.",
-        "04:33:89": "Huawei Technologies Co.,Ltd",
-        "00:00:0C": "Cisco Systems, Inc"
-    }
-
-    return manufacturers.get(hex_digits, "Unknown")
+# Juan Gonzalez (juango4)
+# COP2002-0M1
+# August 27, 2024
+# Exercise 3: If statements
+# This program determines the manufacturer for a NIC card provided a MAC address by the user.
 
 def main():
-    print("           MAC Manufacturer Program")
+    addresses=["00:00:17", "00:07:E9", "04:27:28",  # Valid hex addresses of the NIC cards
+               "04:26:65", "04:33:89", "00:00:0C"]
+    manufacturers=["Oracle", "Intel Corporation", "Microsoft Corporation",  # Manufacturers of the NIC cards
+                   "Apple, Inc.", "Huawei Technologies Co., Ltd", "Cisco Systems, Inc"]
+
+    # Program title with dashes
+    print("MAC Manufacturer Program")
     print("------------------------\n")
-    
-    hex_digits = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ").strip()
 
-    manufacturer = get_manufacturer(hex_digits)
-    
-    print(f"For {hex_digits} the MAC manufacturer is {manufacturer}.")
+    userInput=input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")  # Prompt user for input
 
+    checkingInput=True  # Flag to allow the while loop to run, eventually evaluates to False
 
-if __name__ == "__main__":
+    while checkingInput:
+        if(userInput in addresses):     # Only lands in this if statement if user input exists within addresses
+            print(f"For {userInput} the MAC manufacturer is {manufacturers[addresses.index(userInput)]}.")
+            checkingInput=False    # Changes status of condition for while loop, allowing to break out
+        else:
+            print(f"For {userInput} the MAC manufacturer is Unknown.")
+            checkingInput=False    # Changes status of condition for while loop, allowing to break out
+
+if(__name__=="__main__"):
     main()
