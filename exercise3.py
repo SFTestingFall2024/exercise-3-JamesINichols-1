cb397e111af517385b8c1fa3dffafa5ac0660f79
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-30 16:58:34 -0400

    Delaney M

--- exercise3.py
+++ exercise3.py
@@ -1,34 +1,43 @@
-# dictionary of manufacturers
-manufacturers = {
-    "00:00:17": "Oracle",
-    "00:07:E9": "Intel Corporation",
-    "04:27:28": "Microsoft Corporation",
-    "04:26:65": "Apple, Inc.",
-    "04:33:89": "Huawei Technologies Co.,Ltd",
-    "00:00:0C": "Cisco Systems, Inc"
-}
+#Exercise 3: If Statements
 
-def get_manufacturer(oui):
-    """Retrieve the manufacturer based on the OUI."""
-    return manufacturers.get(oui.upper(), "Manufacturer not found")
+#COP2002 0M1
+#9/22/24
+#This code asks the user for the first 6 hex digits formatted as XX:XX:XX and provides the manufacturer.
 
 def main():
-    print("NIC Card Manufacturer Lookup")
-    
-    # Prompt user for input
-    user_input = input("Enter the first 6 hex digits formatted as XX:XX:XX: ")
 
-    # Validate the input format
-    if (len(user_input) == 8 and
-        user_input[2] == ':' and
-        user_input[5] == ':' and
-        all(c in '0123456789ABCDEFabcdef' for c in user_input.replace(':', ''))):
-        
-        manufacturer = get_manufacturer(user_input[:8])  # Extract the OUI
-        print("\nResult:")
-        print(f"Manufacturer: {manufacturer}")
-    else:
-        print("Invalid format. Please enter the OUI in the format XX:XX:XX.")
+    hexcode = str(input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): "))
 
-if __name__ == "__main__":
+    hexcode1 = "00:00:17"
+    hexcode2= "00:07:E9"
+    hexcode3= "04:26:28"
+    hexcode4 = "04:26:65"
+    hexcode5 = "04:33:89"
+    hexcode6 = "00:00:0C"
+
+# If, elif and else statements check the equality of the hexcode entered.
+# If the hexcode value is listed it prints the manufacturer.
+# If the value entered is not one listed Manufacturer Unknown is printed.
+    if hexcode == hexcode1:
+        print ("For 00:00:17 the MAC manufacturer is Oracle.")
+
+    elif hexcode == hexcode2:
+        print ("For 00:07:E9 the MAC manufacturer is Intel Coperpration.")
+
+    elif hexcode == hexcode3:
+        print ("For 04:27:28 the MAC manufacturer is Microsoft Corporation.")
+
+
+    elif hexcode == hexcode4:
+        print ("For 04:26:65 the MAC manufacturer is Apple, Inc.")
+
+    elif hexcode == hexcode5:
+        print ("For 04:33:89 the MAC manufacturer is Huawei Technologies Co.,Ltd.")
+
+    elif hexcode == hexcode6:
+        print ("For 00:00:0C the MAC manufacturer is Cisco Systems, Inc.")
+
+    else: print ("<Not valid value or not found> Manufacturer Unknown")
+
+if(__name__=="__main__"):
     main()
