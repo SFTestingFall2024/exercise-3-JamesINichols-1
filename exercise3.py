8b743f800283965ef91311a3f4fc41afc9c58539
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-30 06:58:22 -0400

    Garrett S

--- exercise3.py
+++ exercise3.py
@@ -1,36 +1,19 @@
-# Emma Whitworth (EmmaBean004)
-# COP2002-0T1
-# September 22,2024
-# MAC Finder
-# Identifies the manufacturer of a given MAC
-
 def main():
-    # Indent code you write to go in here
-    Hexlist = ["00:00:17",
-               "00:07:E9",
-               "04:27:28",
-               "04:26:65",
-               "04:33:89",
-               "00:00:0C",
-               "<Not valid value or not found>"]
-    Manlist = ["Oracle",
-                    "Intel Corporation",
-                    "Microsoft Corporation",
-                    "Apple,Inc.",
-                    "Huawei Technologies Co.,Ltd",
-                    "Cisco Systems, Inc",
-                    "Unknown"]
-    UserHex = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
-    n = 0
-    while (n<6):
-        if(UserHex==Hexlist[n]):
-            print("For "+UserHex+" the MAC manufacturer is "+Manlist[n])
-            break
-        else:
-            n+=1
-    if (n>=6):
-        print("For "+Hexlist[n]+" the MAC manufacturer is "+Manlist[n])
-    
+     mac_manufacturers = {
+         "00:00:17": "Oracle",
+         "00:07:E9": "Intel Corporation",
+         "04:27:28": "Microsoft Corporation",
+         "04:26:65": "Apple, Inc.",
+         "04:33:89": "Huawei Technologies Co.,ltd",
+         "oo:00:0C": "Cisco Systems, Inc"
+ 
+     }
+     print("           MAC Manufacturer Program")
+     print("------------------------")
+     print()
+     mac_prefix = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+     manufacturer = mac_manufacturers.get(mac_prefix, "Unknown")
+     print(f"\nFor {mac_prefix} the MAC manufacturer is {manufacturer}.")
 if(__name__=="__main__"):
-    print("MAC Manufacturer Program \n")
-    main()
+     main()
+
