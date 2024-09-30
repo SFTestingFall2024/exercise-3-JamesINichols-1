ae6eb863589df77e24c9cc7750df1fc47a3f077e
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-30 07:19:32 -0400

    Ryan C

--- exercise3.py
+++ exercise3.py
@@ -1,30 +1,28 @@
-# Jason DeGroff (JasonDeGroff)
-# COP2002-0M1
-# September 22,2024
-# MAC Manufacturer Program
-# This program takes user input in the format of XX:XX:XX hex values on a NIC card and provides the manufacturer of the card.
+# SFC-COMP-RC
+# COP2002.0M1
+# September 20, 2024
+# Exercise 3: If statements 
+# This program uses a array of known MAC address to take to the user input first 6 hex values of a MAC address to return the manufacturer of a NIC
 
 def main():
-    # Set list of MAC address and Manufactures. These are constants as they should not change during this program
-    MAC_ADDRESSES = ["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C",]
-    MANUFACTURERS = ["Oracle", "Intel Corporation", "Microsoft Corporation", "Apple, Inc.", "Hauwei Technologies Co.,Ltd", "Cisco Systems, Inc", "Unknown"]
-    
-    # Begins display of program and ask for user input
-    print("MAC Manufacturer Program")
-    print("------------------------\n")
-    macAddress = input("Enter the first 6 hex vaules of the MAC address (format as XX:XX:XX): ")
-    
-    #here we use a while loop to go through the MAC_ADDRESSES. If the user input matches the address it leaves the loop, otherwise it itterates the index and trys the next index in the MAC_ADDRESSES list
-    i = 0
-    while i < len(MAC_ADDRESSES):
-        if macAddress == MAC_ADDRESSES[i]:
+
+    # Known values to retrive data from, stored as arrays
+    knownHex=["00:00:17","00:07:E9","04:27:28","04:26:65","04:33:89","00:00:0C"]
+    manufactures=["Oracle","Intel Corporation","Microsoft Corporation","Apple, Inc.","Huawei Technologies Co.,Ltd","Cisco Systems, Inc"]
+
+    # Display a title and Request the user's input stored as macInput
+    macInput=input("MAC Manufactuer Program\n-----------------------\n\nEnter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+
+    for i in range(len(knownHex)): # Loop for the number of items in the Array "knownHex"
+
+        if( knownHex[i] == macInput): # Check if the user input matches a known hex value
+            print(f"For {macInput} the MAC manufacturer is {manufactures[i]}") # Output the user's input and manufacturer of the NIC
             break
-        i+=1
-    
-    #This prints the manufacture for the user input MAC ADDRESS
-    print(f"For {macAddress} the MAC manufacturer is {MANUFACTURERS[i]}.")
+
+    else:
+        print(f"For {macInput} the MAC manufacturer is Unkown") # If the input doesn't match a known MAC address, the output the user's input and Unkown.
 
 
-    
+# Call the main() function
 if(__name__=="__main__"):
     main()
