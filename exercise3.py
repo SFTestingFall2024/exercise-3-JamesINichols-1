fc2a55ff0ad332d88a06e2b76324a9d7819bb08d
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-30 00:05:42 -0400

    Sebastian I

--- exercise3.py
+++ exercise3.py
@@ -1,30 +1,26 @@
-# Juan Gonzalez (juango4)
-# COP2002-0M1
-# August 27, 2024
-# Exercise 3: If statements
-# This program determines the manufacturer for a NIC card provided a MAC address by the user.
+#Sebastian Ildefonso
+#Professor Nichols
+#COP2002.0M1
+#9/22/24
+#This program searches and gives MAC manufactueres based of a Hex code the user inputs
 
-def main():
-    addresses=["00:00:17", "00:07:E9", "04:27:28",  # Valid hex addresses of the NIC cards
-               "04:26:65", "04:33:89", "00:00:0C"]
-    manufacturers=["Oracle", "Intel Corporation", "Microsoft Corporation",  # Manufacturers of the NIC cards
-                   "Apple, Inc.", "Huawei Technologies Co., Ltd", "Cisco Systems, Inc"]
 
-    # Program title with dashes
-    print("MAC Manufacturer Program")
-    print("------------------------\n")
+#Variables
+codeHex = input("Enter the first 6 hex valuse of the MAC address (format as XX:XX:XX): ")
 
-    userInput=input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")  # Prompt user for input
+#Statements that search using user input
+if(codeHex == "00:00:17"):
+    print("The MAC manufacturer is Oracle.")
+elif(codeHex == "00:07:E9"):
+    print("The MAC manufacturer is Intel Corporation.")
+elif(codeHex == "04:27:28"):
+    print("The MAC manufacturer is Microsoft Corporation.")
+elif(codeHex == "04:26:65"):
+    print("The MAC manufacturer is Apple, Inc.")
+elif(codeHex == "04:33:89"):
+    print("The MAC manufacturer is Huawei Technologies Co.,Ltd.")
+elif(codeHex == "00:00:0C"):
+    print("The MAC manufacturer is Cisco Systems, Inc.")
 
-    checkingInput=True  # Flag to allow the while loop to run, eventually evaluates to False
-
-    while checkingInput:
-        if(userInput in addresses):     # Only lands in this if statement if user input exists within addresses
-            print(f"For {userInput} the MAC manufacturer is {manufacturers[addresses.index(userInput)]}.")
-            checkingInput=False    # Changes status of condition for while loop, allowing to break out
-        else:
-            print(f"For {userInput} the MAC manufacturer is Unknown.")
-            checkingInput=False    # Changes status of condition for while loop, allowing to break out
-
-if(__name__=="__main__"):
-    main()
+else:
+        print("Unknown")
