a003442841f006f252eb4be721c8184d19497008
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-29 23:53:22 -0400

    Ekaterina

--- exercise3.py
+++ exercise3.py
@@ -1,30 +1,25 @@
-## Sebastian Brant
-## F24 COP2002.0M1
-## 9/22/24
+def get_manufacturer(hex_digits):
+    manufacturers = {
+        "00:00:17": "Oracle",
+        "00:07:E9": "Intel Corporation",
+        "04:27:28": "Microsoft Corporation",
+        "04:26:65": "Apple, Inc.",
+        "04:33:89": "Huawei Technologies Co.,Ltd",
+        "00:00:0C": "Cisco Systems, Inc"
+    }
+
+    return manufacturers.get(hex_digits, "Unknown")
 
 def main():
-    #Two lists, for hex codes and manufacturer
-    hex_codes=["00:00:17","00:07:E9","04:27:28","04:26:65","04:33:89","00:00:0C"]
-    manufacturer=["Oracle","Intel Corporation","Microsoft Corporation","Apple, Inc.","Huawei Technologies Co.,Ltd","Cisco Systems, Inc"]
+    print("           MAC Manufacturer Program")
+    print("------------------------\n")
+    
+    hex_digits = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ").strip()
 
-    #Input for user, which then runs input through if, elif, else, checking the hex_codes list and printing the corresponding manufacturer
-    print("MAC Manufacturer Program\n------------------------\n")
-    hex_code_input=input("Enter the first 6 values of the MAC address (format as XX:XX:XX): ")
-    if hex_code_input==hex_codes[0]:
-        print(manufacturer[0])
-    elif hex_code_input==hex_codes[1]:
-        print(manufacturer[1])
-    elif hex_code_input==hex_codes[2]:
-        print(manufacturer[2])
-    elif hex_code_input==hex_codes[3]:
-        print(manufacturer[3])
-    elif hex_code_input==hex_codes[4]:
-        print(manufacturer[4])
-    elif hex_code_input==hex_codes[5]:
-        print(manufacturer[5])
-    else:
-        print("Unknown")
+    manufacturer = get_manufacturer(hex_digits)
+    
+    print(f"For {hex_digits} the MAC manufacturer is {manufacturer}.")
 
-#Main function check
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
