53233eb69208f89eead05e82d19d7a38047c9db6
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-30 23:21:52 -0400

    Brody B

--- exercise3.py
+++ exercise3.py
@@ -1,44 +1,23 @@
-# Andrea Camaratta (A-Camaratta on Github)
-# Course ID: COP2002.0M1
-# Date Created: 9/23/2024
-# Title: Exercise 3
-# This program will provide a resource in which a user can input a MAC address and retrieve the manufactuer
+#Brody Burtis (Brody-Burtis)
+#COP2002-0M1
+#September 22, 2024
+#exercise3
+#To determine who the manufacturer for the NIC card is
 
-# In this section we set up the title section
-title_1 = "mac "
-title_2 = "manufacturer program"
-title_1 = title_1.upper()
-title_2 = title_2.title()
-introduction = (title_1+title_2)
-print(introduction)
-
-#we are going to print the dashes afterword
-print(f"-"*26)
-
-# We now define the main function of the program
-def main():
-    prompt = "\nEnter the first 6 hex values of the MAC address (format as XX:XX:XX): "
-    hex_digits = input(prompt)
-
-# Here we are defining the values of the manufacturer based upon the user's input in hex_digits
-    if hex_digits == "00:00:17":
-        manufacturer = "Oracle"
-    elif hex_digits == "00:07:E9":
-        manufacturer = "Intel Corporation"
-    elif hex_digits == "04:27:28":
-        manufacturer = "Microsoft Corporation"
-    elif hex_digits == "04:26:65":
-        manufacturer = "Apple, Inc."
-    elif hex_digits == "04:33:89":
-        manufacturer = "Huawei Technologies Co.,Ltd"
-    elif hex_digits == "00:00:0C":
-        manufacturer = "Cisco Systems, Inc"
-    else:
-        manufacturer = "not found"
-    
-    # Here we are attaching the values from the user input to print the result
-    print(f"For {hex_digits} the MAC manufacturer is {manufacturer}.")
-
-
-if(__name__ == "__main__"):
-    main()
+#input statement that allows you to type in the NIC card to recieve information on who the manufactuerer is
+NIC = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+#If elif statement that determines who the manufactuerer for the NIC card is, if unknown then gives an unknown number
+if NIC == "00:00:17":
+       print("For 00:00:17 the MAC manufacturer is Oracle.")
+elif NIC == "00:07:E9":
+       print("For 00:00:E9 the MAC manufacturer is Intel Corporation.")
+elif NIC == "04:27:28":
+       print("For 04:27:28 the MAC manufacturer is Microsoft Corporation.")
+elif NIC == "04:26:65":
+       print("For 04:27:28 the MAC manufacturer is Apple,Inc.")
+elif NIC == ("04:33:89"):
+       print("For 04:33:89 the MAC manufacturer is Huawei Technologies Co.,Ltd.")
+elif NIC == "00:00:0C":
+       print("For 00:00:0C the MAC manufacturer is Cisco Systems, Inc.")
+else:
+       print("Unknown")
