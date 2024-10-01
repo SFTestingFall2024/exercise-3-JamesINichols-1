be201f81f0253650b17dfcfad58188bfd9a24e84
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-09-30 23:30:44 -0400

    John Mc

--- exercise3.py
+++ exercise3.py
@@ -1,23 +1,30 @@
-#Brody Burtis (Brody-Burtis)
-#COP2002-0M1
-#September 22, 2024
-#exercise3
-#To determine who the manufacturer for the NIC card is
+def get_manufacturer(mac_prefix):
+    # Dictionary of MAC prefix to manufacturer
+    mac_database = {
+        "00:00:17": "Oracle",
+        "00:07:E9": "Intel Corporation",
+        "04:27:28": "Microsoft Corporation",
+        "04:26:65": "Apple, Inc.",
+        "04:33:89": "Huawei Technologies Co.,Ltd",
+        "00:00:0C": "Cisco Systems, Inc",
 
-#input statement that allows you to type in the NIC card to recieve information on who the manufactuerer is
-NIC = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
-#If elif statement that determines who the manufactuerer for the NIC card is, if unknown then gives an unknown number
-if NIC == "00:00:17":
-       print("For 00:00:17 the MAC manufacturer is Oracle.")
-elif NIC == "00:07:E9":
-       print("For 00:00:E9 the MAC manufacturer is Intel Corporation.")
-elif NIC == "04:27:28":
-       print("For 04:27:28 the MAC manufacturer is Microsoft Corporation.")
-elif NIC == "04:26:65":
-       print("For 04:27:28 the MAC manufacturer is Apple,Inc.")
-elif NIC == ("04:33:89"):
-       print("For 04:33:89 the MAC manufacturer is Huawei Technologies Co.,Ltd.")
-elif NIC == "00:00:0C":
-       print("For 00:00:0C the MAC manufacturer is Cisco Systems, Inc.")
-else:
-       print("Unknown")
+        # Add more manufacturers as needed
+    }
+    
+    # Check if the provided prefix exists in the database
+    return mac_database.get(mac_prefix.upper(), "Unknown Manufacturer")
+
+def main():
+    # Ask user for input
+    mac_input = input("Enter the first 6 hex digits of the MAC address (formatted as XX:XX:XX): ")
+
+    # Validate the format
+    if len(mac_input) == 8 and mac_input[2] == ':' and mac_input[5] == ':' and all(c in "0123456789ABCDEFabcdef:" for c in mac_input):
+        manufacturer = get_manufacturer(mac_input)
+        print(f"Manufacturer: {manufacturer}")
+    else:
+        print("Invalid format. Please use XX:XX:XX.")
+
+if __name__ == "__main__":
+    main()
+    
