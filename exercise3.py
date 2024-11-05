23ae7322f2055b610d57f445c9759c3721ae90ae
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-11-05 11:10:58 -0500

    Aidan R

--- exercise3.py
+++ exercise3.py
@@ -1,62 +1,40 @@
-# Tyler Santos (santost12)
-# COP2002-OM1
-# September 18, 2024
-# Exercise 3: If statements
-# Program to determine manufacturer for a NIC card
+#Aidan Riffee
+#F24 COP2002.0M1: PROGRAM LOGIC
+#9/21/24
+#Exercise 3: If-statements
+#This program requires user input in the form of hex digits and outputs
+#the corresponding manufacturer.
 
-
-# Takes macInput string and returns manufacturer as string.
-def lookupMac(macInput):
-    macInput = macInput.upper() # Convert to uppercase
-
-    # Check input for these prefixes.
-    # And if there's no match, return Unknown.
-    if macInput == "00:00:17":
-        return "Oracle"
-    elif macInput == "00:07:E9":
-        return "Intel Corporation"
-    elif macInput == "04:27:28":
-        return "Microsoft Corporation"
-    elif macInput == "04:26:65":
-        return "Apple, Inc."
-    elif macInput == "04:33:89":
-        return "Huawei Technologies Co.,Ltd."
-    elif macInput == "00:00:0C":
-        return "Cisco Systems, Inc."
-    else:
-        return "Unknown"
-
-
-# Takes macInput string and checks if is 8 characters.
-# Returns 0 for valid input.
-def validateInputLen(macInput):
-
-    # Eight characters exactly includes the colon.
-    if len(macInput) < 8:
-        print("Error: Not enough characters entered.")
-        return -1
-    elif len(macInput) > 8:
-        print("Error: Too many characters entered.")
-        return -2 
-    else:
-        return 0
-
-
-# Main function, where the user is prompted for input.
 def main():
+
     print("MAC Manufacturer Program")
-    print("------------------------\n")
+    print("-------------------------")
+    
+    #lists for the hex digits and the manufacturers
 
-    macInput = str(input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): "))
+    hexDigits=['00:00:17', '00:07:E9', '04:27:28', '04:26:65', '04:33:89', '00:00:0C']
+    manufacturers=['Oracle', 'Intel Corporation', 'Microsoft Corporation', 'Apple, Inc', 'Huawei Technologies Co.,Ltd', 'Cisco Systems, Inc', 'Unknown']
+    
+    #vars and user prompt
+    index=0
+    code=""
+    address=input("\nEnter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+    
+    #for loop to identify the index of the input value in the list hexDigits
 
-    # Require 8 characters. Ask for input again if too many or too few.
-    while (validateInputLen(macInput) != 0):
-        macInput = str(input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): "))
+    for digit in hexDigits:
+        if(digit==address):
+            index=hexDigits.index(digit)
+            code=digit
+  
+    #determines if the input value was recognizable
+  
+    if (code==""):
+        print(f"\nFor {address} the MAC manufacturer is {manufacturers[-1]}.")
+    else:
+        print(f"\nFor {address} the MAC manufacturer is {manufacturers[index]}.")
 
-    # Get manufacturer and then print.
-    manufacturer = lookupMac(macInput)
-    print(f"For {macInput} the MAC manufacturer is {manufacturer}")
+#function call
 
-
-if (__name__=="__main__"):
+if(main==main):
     main()
