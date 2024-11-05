9d3045d5ecca38e0ce7b81f751c966eaa1c4cdcf
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2024-11-05 12:09:27 -0500

    James J

--- exercise3.py
+++ exercise3.py
@@ -1,25 +1,32 @@
+#James Johnson f24 COP2002-0m1
+#This program will ask the user to input a 6 digit hex number from a manufacturer and then tell the user who the manufacturer is.
+
+def get_mac_manufacturer(mac):
+    # Dictionary for hex values
+    manufacturers = {
+        "00:00:17": "Oracle",
+        "00:07:E9": "Intel Corporation",
+        "04:27:28": "Microsoft Corporation",
+        "04:26:65": "Apple, Inc.",
+        "04:33:89": "Huawei Technologies Co.,Ltd",
+        "00:00:0C": "Cisco Systems, Inc"
+    }
+    
+    # Return the manufacturer or "Unknown" if manufacturer is not found
+    return manufacturers.get(mac, "Unknown")
+
 def main():
-    # List of hex digits and corresponding manufacturers
-    hex_codes = ["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C"]
-    manufacturers = [
-        "Oracle",
-        "Intel Corporation",
-        "Microsoft Corporation",
-        "Apple, Inc.",
-        "Huawei Technologies Co.,Ltd",
-        "Cisco Systems, Inc."
-    ]
+    print("           MAC Manufacturer Program")
+    print("------------------------\n")
 
-    # Ask user for input
-    user_input = input("Please enter the first 6 hex digits (formatted as XX:XX:XX): ")
+    # Prompt the user for the first 6 hex digits
+    mac_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+    
+    # Get the manufacturer name
+    manufacturer = get_mac_manufacturer(mac_input)
+    
+    # Displays the result to the user
+    print(f"\nFor {mac_input} the MAC manufacturer is {manufacturer}.")
 
-    # Search for the manufacturer
-    if user_input in hex_codes:
-        index = hex_codes.index(user_input)
-        print(f"Hex Digits\tManufacturer\n{user_input}\t{manufacturers[index]}")
-    else:
-        print(f"Hex Digits\tManufacturer\n{user_input}\tUnknown")
-
-# This is so that the main function runs only if this file is executed 
 if __name__ == "__main__":
     main()
